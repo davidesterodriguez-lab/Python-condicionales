339623892165a4aff9afe559aa49f346d1f9ead3
davidesterodriguez-lab <davidesterodriguez@gmail.com>
2025-11-05 07:22:15 -0500

    Delete Algoritmo #23 - Mejor programa.py

--- Algoritmo #23 - Mejor programa.py
+++
@@ -1,8 +0,0 @@
-lenguaje = "Java"
-
-if lenguaje == "Python":
-    print("Estoy de acuerdo, Python es el mejor")
-elif lenguaje == "Java":
-    print("No me gusta, java no es chimbita")
-else:
-    print("Ningun otro lenguaje supera a Python")
