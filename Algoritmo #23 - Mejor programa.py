44559cc22b53c06a4e606f7fa8d3dd5a360ad8f8
davidesterodriguez-lab <davidesterodriguez@gmail.com>
2025-11-05 06:48:46 -0500

    Add files via upload

---
+++ Algoritmo #23 - Mejor programa.py
@@ -0,0 +1,8 @@
+lenguaje = "Java"
+
+if lenguaje == "Python":
+    print("Estoy de acuerdo, Python es el mejor")
+elif lenguaje == "Java":
+    print("No me gusta, java no es chimbita")
+else:
+    print("Ningun otro lenguaje supera a Python")
